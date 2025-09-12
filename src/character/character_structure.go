5d1f74f1373cced8047415adea807fb94e7d2ae8
ThibaudS <thibaud.sellier@ynov.com>
2025-09-12 12:29:29 +0200

    Finalisation du code d'enregistrement et de vérification de la classe

--- src/character/character_structure.go
+++ src/character/character_structure.go
@@ -10,10 +10,13 @@ func main() {
 }
 
 func setInfo() string {
-	//Nom
+	//Nom (Fonctionnel)
 	var inputName string
 	fmt.Print("Écris le nom de ton nouveau personnage : ")
 	fmt.Scan(&inputName)
+	for i := 0; i <= 20; i++ {
+		fmt.Println()
+	}
 	Name := ""
 	for VerifName := 0; VerifName < len(inputName); VerifName++ {
 		if (inputName[VerifName] >= 'A' && inputName[VerifName] <= 'Z') || (inputName[VerifName] >= 'a' && inputName[VerifName] <= 'z') || inputName[VerifName] == ' ' {
@@ -27,27 +30,32 @@ func setInfo() string {
 		}
 	}
 
-	//Classe
+	//Classe (fonctionnel)
 	var inputClass string
-	fmt.Println("Choisis la classe de ton personnage :")
-	fmt.Println("1 - Guerrier")
-	fmt.Println("2- Mage")
-	fmt.Scan(&inputClass)
 	Class := ""
-	for Class == "Guerrier" || Class == "Mage" {
+	for comptInvalidClass := 1; !(Class == "Guerrier" || Class == "Mage"); comptInvalidClass++ {
+		fmt.Println("Choisis la classe de ton personnage :")
+		fmt.Println("1 - Guerrier")
+		fmt.Println("2- Mage")
+		fmt.Scan(&inputClass)
+		for i := 0; i <= 20; i++ {
+			fmt.Println()
+		}
 		switch inputClass {
 		case "1":
 			Class = "Guerrier"
 		case "2":
 			Class = "Mage"
 		default:
-			fmt.Println("Invalid Class")
-			fmt.Scan(&inputClass)
+			fmt.Println("// Invalid Class // x", comptInvalidClass)
+
 		}
 	}
 
-	//retours
-	fmt.Println()
+	//retour d'infos
+	for i := 0; i <= 20; i++ {
+		fmt.Println()
+	}
 	fmt.Println("Informations de votre nouveau personnage :")
 	fmt.Println("Nom :", Name)
 	fmt.Println("Classe :", Class)
