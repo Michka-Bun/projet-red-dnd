3b70d1aaea58589509cf989fc650c5f9892cf3bc
ThibaudS <thibaud.sellier@ynov.com>
2025-09-12 12:49:25 +0200

    Ajout de l'assignation et affichage des stats en fonction de la classe choisie + modification de l'affichage d'erreur du nom

--- src/character/character_structure.go
+++ src/character/character_structure.go
@@ -17,23 +17,28 @@ func setInfo() string {
 	for i := 0; i <= 20; i++ {
 		fmt.Println()
 	}
+	var ComptInvalidName = 1
 	Name := ""
 	for VerifName := 0; VerifName < len(inputName); VerifName++ {
 		if (inputName[VerifName] >= 'A' && inputName[VerifName] <= 'Z') || (inputName[VerifName] >= 'a' && inputName[VerifName] <= 'z') || inputName[VerifName] == ' ' {
 			Name = inputName
 		} else {
-			fmt.Println("// Invalid Name //")
+			fmt.Println("// Invalid Name // x", ComptInvalidName)
 			fmt.Println("Choissis un autre nom :")
 
 			fmt.Scan(&inputName)
 			VerifName = -1
+			ComptInvalidName = ComptInvalidName + 1
+			for i := 0; i <= 20; i++ {
+				fmt.Println()
+			}
 		}
 	}
 
 	//Classe (fonctionnel)
 	var inputClass string
 	Class := ""
-	for comptInvalidClass := 1; !(Class == "Guerrier" || Class == "Mage"); comptInvalidClass++ {
+	for ComptInvalidClass := 1; !(Class == "Guerrier" || Class == "Mage"); ComptInvalidClass++ {
 		fmt.Println("Choisis la classe de ton personnage :")
 		fmt.Println("1 - Guerrier")
 		fmt.Println("2- Mage")
@@ -47,9 +52,27 @@ func setInfo() string {
 		case "2":
 			Class = "Mage"
 		default:
-			fmt.Println("// Invalid Class // x", comptInvalidClass)
+			fmt.Println("// Invalid Class // x", ComptInvalidClass)
 
 		}
+	}
+
+	//stats en fonction de la classe
+	HPmax := 0
+	HP := 0
+	Level := 1
+	InventorySlot := 0
+	switch Class {
+	case "Guerrier":
+		HPmax = 100
+		HP = 100
+		Level = 1
+		InventorySlot = 5
+	case "Mage":
+		HPmax = 75
+		HP = 75
+		Level = 1
+		InventorySlot = 10
 	}
 
 	//retour d'infos
@@ -57,8 +80,13 @@ func setInfo() string {
 		fmt.Println()
 	}
 	fmt.Println("Informations de votre nouveau personnage :")
-	fmt.Println("Nom :", Name)
-	fmt.Println("Classe :", Class)
+	fmt.Println("Name :", Name)
+	fmt.Println("Class :", Class)
+	fmt.Println("HP max :", HPmax)
+	fmt.Println("HP :", HP)
+	fmt.Println("Level :", Level)
+	fmt.Println("Inventory slots :", InventorySlot)
+
 	return ""
 }
 
