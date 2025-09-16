3900ae4c8b2f5940477e6470bbfc8c5539035abb
ThibaudS <thibaud.sellier@ynov.com>
2025-09-16 11:48:44 +0200

    Création de monster.go avec la func ChooseMonster pour définir le monstre a combattre, et DisplayMonsterInfos pour afficher les stats du monstre pendant le combat. Rajout des dégats de poison sur les monstre. Modification de l'affichage des HP dans DisplayInfo

--- src/useitems.go
+++ src/useitems.go
@@ -12,13 +12,22 @@ func TakeHealPot(p Player) {
 	}
 }
 
-func PoisonPot(p Player) {
+func PoisonPot(p Player, m Monster) {
 	if p.PoisonEffect > 0 { //Affecter une valeur de poison directement pendant le combat // issue de la structure Player
 		p.HP -= 15
 		p.PoisonEffect -= 1
-		fmt.Println("Tu as pris 15 HP à cause du poison, il te reste", p.PoisonEffect, "tours de poison")
+		fmt.Println("You took 15 HP damage from the poison, you have", p.PoisonEffect, "turn(s) of poison left")
+		if p.HP < 1 {
+			//	dnd.IsDead() //IsDead pas encore définie
+		}
 	}
-	if p.HP < 1 {
-		//	projet_red_dnd.IsDead() //IsDead pas encore définie
+
+	if m.PoisonEffect > 0 { //Affecter une valeur de poison directement pendant le combat // issue de la structure Monster (non faite)
+		m.HP -= 15
+		m.PoisonEffect -= 1
+		fmt.Println("The monster took 15 HP damage from the poison, he has", m.PoisonEffect, "turn(s) of poison left")
+		if m.HP < 1 {
+			//	dnd.MonsterIsDead() //MonsterIsDead pas encore définie
+		}
 	}
 }
